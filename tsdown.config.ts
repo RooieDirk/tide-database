ec532e2e213d8ce2906e656bd79c94a324fc258e
Brandon Keepers <brandon@opensoul.org>
2026-01-12 14:56:35 -0500

    chore: update tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "tsdown";
 import macros from "unplugin-macros/rolldown";
 
 export default defineConfig({
-  entry: ["./src/index.ts"],
+  exports: true,
   dts: true,
+  minify: true,
   format: ["cjs", "esm"],
   sourcemap: true,
-  declarationMap: true,
   target: "es2020",
   platform: "neutral",
   plugins: [macros()],
